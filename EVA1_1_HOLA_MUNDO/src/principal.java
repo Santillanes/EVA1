270f4ff82d0cc5d6615d9201868d4520569c62aa
Santillanes <42648099+Santillanes@users.noreply.github.com>
2018-09-24 18:43:38 -0600

    Final

---
+++ EVA1_1_HOLA_MUNDO/src/principal.java
@@ -0,0 +1,19 @@
+/*
+ * Hola Mundo
+ */
+
+/**
+ *
+ * @author Luis Santillanes - 18550694
+ */
+public class principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo!");
+    }
+    
+}
---
+++ EVA1_3_TIPOS_DATOS/src/principal.java
@@ -0,0 +1,21 @@
+/*
+ * Declaración de arreglos
+ */
+
+/**
+ *
+ * @author Luis Santillanes - 18550694
+ */
+public class principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Arreglo = Lista de variables del mismo tipo
+        int[] iArreglo = new int [1000000];
+        System.out.println("Hola mundo "+iArreglo);
+    }
+    
+}
